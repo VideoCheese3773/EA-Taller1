ae85577ab78e674e8494b801b65d4748aa670599
VideoCheese3773 <eedduxx@gmail.com>
2023-08-04 07:40:19 -0500

    Done API Fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("cyka blyat")
+console.log("cyka blyat");
+
+let APIData = null;
+
+async function loadAPI () {
+    const url = "https://swapi.dev/api/people/";
+    const response = await fetch(url);
+    APIData = await response.json()
+    console.log("APIData",APIData)
+    showAPI()
+}
+
+function showAPI () {
+    console.log("loading data...")
+}
+
+loadAPI()
